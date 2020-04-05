8004764b6876fb01d38c39270c44a641a406c330
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-04-06 02:13:52 +0800

    WIP allow user to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#index'
+  match "/dashboard", to: 'dashboard#index', via: [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
