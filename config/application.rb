1555a8629463006cfe7b9a2fdc898962e1db807f
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-04-08 16:47:08 +0800

    Update game

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ShowoffTechTest
   class Application < Rails::Application
+    config.serve_static_assets = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
