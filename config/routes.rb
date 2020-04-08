730b91eb319e60e1ccd508dbdb77dedc1a62e190
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-04-08 16:39:43 +0800

    Implement users/me page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#index'
   match "/dashboard", to: 'dashboard#index', via: [:get, :post]
-  resources :users, only: [:index, :create]
+  resources :users, only: [:index, :create] do
+    collection do
+      get :me
+    end
+  end
   resources :sessions, only: :create
   resources :widgets, only: [:create, :update, :destroy]
   match '/sign_out', to: 'sessions#destroy', via: [:get, :delete]
